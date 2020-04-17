e0aba15c0e4338133fa8ee6502418f511d1537be
Chetan S P <chetan.suprakash@gmail.com>
2020-04-17 16:42:50 +0530

    initial 1

---
+++ app/src/main/java/tvk/spc/bitbucket_trial/MainActivity.java
@@ -0,0 +1,14 @@
+package tvk.spc.bitbucket_trial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
